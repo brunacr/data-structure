b9539dd926ca4a33f1d7bdb364a6044e25c10c55
brunacr <bruna.leitao.pro@gmail.com>
2024-07-23 11:57:53 -0300

    create display method

--- src/dataStructurePDI/NewStack.java
+++ src/dataStructurePDI/NewStack.java
@@ -41,8 +41,24 @@ public class NewStack { //Define a classe NewStack que representa a pilha
         return data; //Retorna o valor do nó removido do topo da pilha
     }
 
-    public void displayUntil(int limit) {
-        Node current = top;
+    public void display() { //Método que exibe todos os elementos da pilha, indicando quais são o topo e o fundo (bottom)
+        Node current = top; //Inicializa um ponteiro current para percorrer a pilha a partir do topo (top)
+
+        while (current != null) { //Um loop while que percorre todos os nós da pilha até encontrar o final (null)
+            String format = "\t[%3s  ]\n";
+            if (current == top) { //Define uma string de formato para a impressão. Se o nó atual for o topo, adiciona "top" ao formato. Se for o último nó (bottom), adiciona "bottom" ao formato
+                format = "\t[%3s  ] top\n";
+            }
+            if (current.next == null) {
+                format = "\t[%3s  ] bottom\n";
+            }
+            System.out.printf(format, current.data); //Imprime os dados do nó atual usando o formato definido
+            current = current.next; //Avança para o próximo nó na pilha
+        }
+    }
+
+    public void displayUntil(int limit) { //Método que exibe os elementos da pilha até um limite especificado, indicando o topo e o fundo (bottom) quando apropriado
+        Node current = top; //Inicializa um ponteiro current para percorrer a pilha a partir do topo e um contador count para controlar o número de nós exibidos
         int count = 1;
 
         while(count <= limit && current != null) {
@@ -60,29 +76,28 @@ public class NewStack { //Define a classe NewStack que representa a pilha
     }
 
     public void displayInvertedUntil(int limit) {
-        NewStack auxStack = new NewStack();
-        Node current = top;
-        int count = 0;
+        NewStack auxStack = new NewStack(); //Cria uma nova pilha auxiliar
+        Node current = top; //Inicializa a variável 'current' com o nó do topo da pilha original
+        int count = 1; //Inicializa um contador para rastrear o número de nós processados
 
         while (current != null) {
-            auxStack.push(current.data);
-            current = current.next;
+            auxStack.push(current.data); //Empilha os dados do nó atual na pilha auxiliar
+            current = current.next; //Move para o próximo nó na pilha original
         }
 
-        current = auxStack.top;
-        count = 0;
+        current = auxStack.top; //Reposiciona 'current' para o topo da pilha auxiliar
 
-        while (count < limit && current != null) {
-            String format = "\t[%3s  ]\n";
-            if (current == auxStack.top) {
+        while (count <= limit && current != null) {
+            String format = "\t[%3s  ]\n"; //Define o formato padrão da string para impressão
+            if (current == auxStack.top) { //Se 'current' for o nó do topo da pilha auxiliar, altera o formato da string para indicar que é o topo
                 format = "\t[%3s  ] top\n";
             }
-            if (current.next == null) {
+            if (current.next == null) { //Se 'current.next' for nulo, altera o formato da string para indicar que é o final da pilha
                 format = "\t[%3s  ] bottom\n";
             }
-            System.out.printf(format, current.data);
-            current = current.next;
-            count++;
+            System.out.printf(format, current.data); //Imprime os dados do nó atual no formato especificado
+            current = current.next; //Move para o próximo nó na pilha auxiliar
+            count++; //Incrementa o contador
         }
     }
 }
